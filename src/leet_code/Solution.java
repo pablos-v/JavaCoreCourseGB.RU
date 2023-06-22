957027193c35e8afee66c5c39d53f0f7e3789543
Pablos <pablos@list.ru>
2023-06-22 13:36:13 -0300

    leetcode task https://leetcode.com/problems/best-time-to-buy-and-sell-stock solved

--- src/leet_code/Solution.java
+++ src/leet_code/Solution.java
@@ -1,42 +1,22 @@
 package leet_code;
 
-import java.util.*;
 
-/**
- * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']',
- * determine if the input string is valid.
- * <p>
- * An input string is valid if:
- * <p>
- * Open brackets must be closed by the same type of brackets.
- * Open brackets must be closed in the correct order.
- * Every close bracket has a corresponding open bracket of the same type.
- */
 class Solution {
-
-    public boolean isValid(String s) {
-        Map<Character, Character> map = new HashMap<>();
-        map.put(')', '(');
-        map.put('}', '{');
-        map.put(']', '[');
-        Stack<Character> stack = new Stack<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            if (map.containsValue(s.charAt(i))) {
-                stack.push(s.charAt(i));
-            } else {
-                if (!stack.empty() && stack.peek().equals(map.get(s.charAt(i)))) {
-                    stack.pop();
-                } else return false;
-            }
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) return 0;
+        int min = prices[0];
+        int profit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            min = Math.min(prices[i], min);
+            profit = Math.max(profit, prices[i] - min);
         }
-        return stack.empty();
+        return profit;
     }
+
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.isValid("(){}["));
-        System.out.println(s.isValid("(){}[]"));
-        System.out.println(s.isValid("([])"));
+        System.out.println(s.maxProfit(new int[]{4, 6, 7, 3, 1}));
+
     }
 }
