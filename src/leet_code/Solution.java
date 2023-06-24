472330fb23a43c62f7bb9f69e45f1de55f854692
Pablos <pablos@list.ru>
2023-06-24 19:22:07 -0300

    find the length of the longest substring with no more than K distinct characters.

--- src/leet_code/Solution.java
+++ src/leet_code/Solution.java
@@ -1,60 +1,47 @@
 package leet_code;
 
+import java.util.HashSet;
+
 /**
- * Реализуйте функцию, которая выводит в терминал числа в диапазоне от begin до end. При этом:
- * <p>
- * Если число делится без остатка на 3, то вместо него выводится слово Fizz
- * Если число делится без остатка на 5, то вместо него выводится слово Buzz
- * Если число делится без остатка и на 3, и на 5, то вместо числа выводится слово FizzBuzz
- * В остальных случаях выводится само число
- * Функция принимает два параметра (begin и end), определяющих начало и конец диапазона (включительно).
- * Для простоты считаем, что эти параметры являются целыми числами больше нуля.
- * Если диапазон пуст (в случае, когда begin > end), то функция просто ничего не печатает.
+ Given a string, find the length of the longest substring in it with no more than K distinct characters.
+
+ Example 1:
+ Input: String="araaci", K=2
+ Output: 4
+ Explanation: The longest substring with no more than '2' distinct characters is "araa".
+
+ Example 2:
+ Input: String="araaci", K=1
+ Output: 2
+ Explanation: The longest substring with no more than '1' distinct characters is "aa".
+
+ Example 3:
+ Input: String="cbbebi", K=3
+ Output: 5
  */
 class Solution {
-    String CASE3 = "FizzBuzz";
-    String CASE1 = "Fizz";
-    String CASE2 = "Buzz";
 
-//    public void printer(int begin, int end) {
-//        for (int i = begin; i <= end; i++) {
-//            if (i % 3 == 0) {
-//                if (i % 5 == 0) {
-//                    System.out.println(CASE3);
-//                    continue;
-//                }
-//                System.out.println(CASE1);
-//            } else if (i % 5 == 0) {
-//                System.out.println(CASE2);
-//            } else System.out.println(i);
-//        }
-//    }
+    public int countSubstring(String str, int K){
+        int res = 0;
+        if (K <= 0) return res;
+        HashSet<Character> set = new HashSet<>();
 
-//    public void printer2(int begin, int end) {
-//        String fizz = "";
-//        String buzz = "";
-//        for (int i = begin; i <= end; i++) {
-//            fizz = i % 3 == 0 ? "Fizz" : "";
-//            buzz = i % 5 == 0 ? "Buzz" : "";
-//            if (i % 3 == 0 || i % 5 == 0) {
-//                System.out.println(fizz + buzz);
-//            } else System.out.println(i);
-//        }
-//    }
-
-    public void printer3(int begin, int end){
-        for (int i = begin; i <= end; i++)  {
-            String str = "";
-            if (i%3==0) str += "Fizz";
-            if (i%5==0) str += "Buzz";
-            if (str.length() > 0) System.out.println(str);
-            else System.out.println(i);
+        for (int j = 0; j < str.length(); j++) {
+            for (int i = j; i < str.length(); i++) {
+                set.add(str.charAt(i));
+                if (set.size() > K) break;
+                if (set.size() == K) res = Math.max(res, i-j+1);
+            }
+            set.clear();
 
         }
+        return res;
     }
-
     public static void main(String[] args) {
         Solution s = new Solution();
-        s.printer3(1, 22);
+        System.out.println(s.countSubstring("araaci", 2) == 4);
+        System.out.println(s.countSubstring("araaci", 1) == 2);
+        System.out.println(s.countSubstring("fxcbbebi", 3) == 5);
+        System.out.println(s.countSubstring("rrrrr", 1) == 5);
     }
 }
