022e5d0805925a0b972e36394029cfcb65c51416
Pablos <pablos@list.ru>
2023-07-12 17:58:01 -0300

    https://www.designgurus.io/course-play/grokking-the-coding-interview/doc/63ddacd4fcc4ca873d5fbfbc solved

--- src/leet_code/Solution.java
+++ src/leet_code/Solution.java
@@ -1,55 +1,84 @@
 package leet_code;
 
+import java.util.Arrays;
+
 /**
- * Given a binary array, find the index of 0 to be replaced with 1 to get a maximum length sequence of continuous ones.
+ * Given a sorted array, create a new array containing squares of all the numbers of the input array in the sorted order.
  * <p>
- * For example, consider array { 0, 0, 1, 0, 1, 1, 1, 0, 1, 1 }.
- * The index to be replaced is 7 to get a continuous sequence of length 6 containing all 1’s.
+ * Example 1:
+ * Input: [-2, -1, 0, 2, 3]
+ * Output: [0, 1, 4, 4, 9]
+ * <p>
+ * Example 2:
+ * Input: [-3, -1, 0, 1, 2]
+ * Output: [0, 1, 1, 4, 9]
  */
 class Solution {
 
-    public int findIndex(int[] arr) {
-        int len, idx = 0, res = 0, lenPrevious = 0, left = 0;
-        boolean isPreviousZero = false;
+    public int[] squaredArray(int[] arr) {
+//        int[] res = new int[arr.length];
+//        for (int i = 0; i < arr.length; i++) {
+//            res[i] = (int) Math.pow(arr[i], 2);
+//        }
+//        Arrays.sort(res);
+//        return res;
 
-        for (int right = 0; right < arr.length; right++) {
-            if (arr[right] == 0) {
+        int[] res = new int[arr.length];
+        int pointer1 = 0;
+        int pointer2 = 0;
 
-                if (isPreviousZero) {
-
-                    len = right - left;
-                    if (len > lenPrevious) {
-                        res = idx;
-                        lenPrevious = len;
-                    }
-
-                    left = right;
-                    isPreviousZero = false;
-
-                } else {
-                    isPreviousZero = true;
-                    left++;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] >= 0) {
+                res[0] = (int) Math.pow(arr[i], 2);
+                if (i < arr.length - 1) {
+                    pointer2 = i + 1;
                 }
-
-                idx = right;
-
-            } else {
-                isPreviousZero = false;
-            }
-
-            len = right - left + 1;
-            if (len > lenPrevious) {
-                res = idx;
-                lenPrevious = len;
+                if (i > 0) {
+                    pointer1 = i - 1;
+                }
+                break;
             }
         }
+        if (pointer1 == 0 && pointer2 == 0) {
+            int len = arr.length - 1;
+            for (int i = 0; i < arr.length; i++) {
+                res[i] = (int) Math.pow(arr[len--], 2);
+            }
+            return res;
+        }
 
+        for (int i = 1; i < arr.length - 1; i++) {
+            int sq1 = (int) Math.pow(arr[pointer1], 2);
+            int sq2 = (int) Math.pow(arr[pointer2], 2);
+            if (sq1 <= sq2) {
+                res[i] = sq1;
+                if (pointer1 > 0) {
+                    pointer1--;
+                } else {
+                    for (int j = pointer2; j < arr.length; j++) {
+                        res[j] = (int) Math.pow(arr[j], 2);
+                    }
+                    return res;
+                }
+            } else {
+                res[i] = sq2;
+                if (pointer2 < arr.length - 1) {
+                    pointer2++;
+                } else {
+                    for (int j = pointer1; j >= 0; j--) {
+                        res[pointer2++] = (int) Math.pow(arr[j], 2);
+                    }
+                    return res;
+                }
+            }
+
+        }
         return res;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.println(s.findIndex(new int[]{0, 0, 1, 0, 1, 1, 1, 0, 1, 1}));
-        System.out.println(s.findIndex(new int[]{1, 0, 1, 1, 0, 0, 0, 1, 1}));
+        int[] arr = new int[]{-6, 0, 4, 5, 8 ,9};
+        System.out.println(Arrays.toString(s.squaredArray(arr)));
     }
 }
