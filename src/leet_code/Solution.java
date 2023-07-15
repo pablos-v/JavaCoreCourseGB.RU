12b42d07dd414a0cbf54471ff2cd722a8f7b747f
Pablos <pablos@list.ru>
2023-07-15 13:50:59 -0300

    https://www.designgurus.io/course-play/grokking-the-coding-interview/doc/63ddacd4fcc4ca873d5fbfbc solved better

--- src/leet_code/Solution.java
+++ src/leet_code/Solution.java
@@ -16,69 +16,28 @@ import java.util.Arrays;
 class Solution {
 
     public int[] squaredArray(int[] arr) {
-//        int[] res = new int[arr.length];
-//        for (int i = 0; i < arr.length; i++) {
-//            res[i] = (int) Math.pow(arr[i], 2);
-//        }
-//        Arrays.sort(res);
-//        return res;
-
+        int left = 0;
+        int right = arr.length - 1;
         int[] res = new int[arr.length];
-        int pointer1 = 0;
-        int pointer2 = 0;
+        int i = arr.length - 1;
 
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] >= 0) {
-                res[0] = (int) Math.pow(arr[i], 2);
-                if (i < arr.length - 1) {
-                    pointer2 = i + 1;
-                }
-                if (i > 0) {
-                    pointer1 = i - 1;
-                }
-                break;
-            }
-        }
-        if (pointer1 == 0 && pointer2 == 0) {
-            int len = arr.length - 1;
-            for (int i = 0; i < arr.length; i++) {
-                res[i] = (int) Math.pow(arr[len--], 2);
-            }
-            return res;
-        }
-
-        for (int i = 1; i < arr.length - 1; i++) {
-            int sq1 = (int) Math.pow(arr[pointer1], 2);
-            int sq2 = (int) Math.pow(arr[pointer2], 2);
-            if (sq1 <= sq2) {
-                res[i] = sq1;
-                if (pointer1 > 0) {
-                    pointer1--;
-                } else {
-                    for (int j = pointer2; j < arr.length; j++) {
-                        res[j] = (int) Math.pow(arr[j], 2);
-                    }
-                    return res;
-                }
+        while (left < right) {
+            int l = (int) Math.pow(arr[left], 2);
+            int r = (int) Math.pow(arr[right], 2);
+            if (l > r) {
+                res[i--] = l;
+                left++;
             } else {
-                res[i] = sq2;
-                if (pointer2 < arr.length - 1) {
-                    pointer2++;
-                } else {
-                    for (int j = pointer1; j >= 0; j--) {
-                        res[pointer2++] = (int) Math.pow(arr[j], 2);
-                    }
-                    return res;
-                }
+                res[i--] = r;
+                right--;
             }
-
         }
         return res;
     }
 
     public static void main(String[] args) {
         Solution s = new Solution();
-        int[] arr = new int[]{-6, 0, 4, 5, 8 ,9};
+        int[] arr = new int[]{-7, -6, 0, 4, 5, 8, 9};
         System.out.println(Arrays.toString(s.squaredArray(arr)));
     }
 }
