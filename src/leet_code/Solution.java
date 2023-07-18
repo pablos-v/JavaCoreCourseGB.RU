285cae259cab63fe3fce9d952430ea053e85a610
Pablos <pablos@list.ru>
2023-07-18 12:57:24 -0300

    https://xn--80a2ac.xn--p1ai/%D0%BC%D0%B0%D1%82%D0%B5%D1%80%D0%B8%D0%B0%D0%BB%D1%8B/%D0%BC%D0%B5%D1%82%D0%BE%D0%B4-%D0%B4%D0%B2%D1%83%D1%85-%D1%83%D0%BA%D0%B0%D0%B7%D0%B0%D1%82%D0%B5%D0%BB%D0%B5%D0%B9/ solved

--- src/leet_code/Solution.java
+++ src/leet_code/Solution.java
@@ -1,68 +1,30 @@
 package leet_code;
 
-import java.util.Arrays;
-
 /**
- * Given an array containing 0s, 1s and 2s, sort the array in-place. You should treat numbers of the array as
- * objects, hence, we can’t count 0s, 1s, and 2s to recreate the array.
- * The flag of the Netherlands consists of three colors: red, white and blue; and since our input array also
- * consists of three different numbers that is why it is called Dutch National Flag problem.
- * <p>
- * Example 1:
- * Input: [1, 0, 2, 1, 0]
- * Output: [0 0 1 1 2]
- * <p>
- * Example 2:
- * Input: [2, 2, 0, 1, 2, 0]
- * Output: [0 0 1 2 2 2 ]
+ * Для заданного отсортированного в возрастающем порядке массива A целых чисел длины n
+ * и целого числа X определить, существует ли в нем два различных элемента A[i] и A[j],
+ * такие что A[i] + A[j] = X.
  */
 class Solution {
 
-    static int TWO = 2;
-    static int ZERO = 0;
-
-    public static void dnf(int[] arr) {
-        int c = 0;
-        int fin = arr.length - 1;
-        int start = 0;
-        for (int i = 0; i <= fin; i++) {
-            System.out.printf("for %d%n", c++);
-            if (arr[i] == TWO) {
-                int j = fin;
-                while (j > i) {
-            System.out.printf("while-2 %d%n", c++);
-                    if (arr[j] != TWO) {
-                        int tmp = arr[j];
-                        arr[j] = TWO;
-                        arr[i] = tmp;
-                        fin--;
-                        break;
-                    }
-                    fin--;
-                    j--;
-                }
+    public static boolean isNumInArray(int[] arr, int X) {
+        int i = 0;
+        int j = arr.length - 1;
+        while (i != j) {
+            if (arr[i] + arr[j] == X) {
+                System.out.printf("i = %d, j = %d ", arr[i], arr[j]);
+                return true;
             }
-            if (arr[i] == ZERO) {
-                int j = start;
-                while (j < i) {
-            System.out.printf("while-0 %d%n", c++);
-                    if (arr[j] != ZERO) {
-                        int tmp = arr[j];
-                        arr[j] = ZERO;
-                        arr[i] = tmp;
-                        start = j + 1;
-                        break;
-                    }
-                    j++;
-                }
-            }
+            if (arr[i] + arr[j] > X) j--;
+            else i++;
         }
+        return false;
+
     }
 
+
     public static void main(String[] args) {
-//        Solution s = new Solution();
-        int[] arr = new int[]{1, 2, 0, 1, 2, 1, 1, 2, 0, 1, 2, 1,1, 2, 0, 1, 2, 1, 1, 2, 0, 1, 2, 1};
-        Solution.dnf(arr);
-        System.out.println(Arrays.toString(arr));
+        int[] arr = new int[]{-1, 3, 6, 7, 11, 15};
+        System.out.println(Solution.isNumInArray(arr, 13));
     }
 }
