85fb764198e1203d2171ce6b847462e0b0cfb3c9
Pablos <pablos@list.ru>
2023-07-23 14:27:13 -0300

    https://leetcode.com/problems/middle-of-the-linked-list solved

--- src/leet_code/Solution.java
+++ src/leet_code/Solution.java
@@ -1,49 +1,61 @@
 package leet_code;
 
 /**
- * Write an algorithm to determine if a number n is happy.
+ * Given the head of a Singly LinkedList, write a method to return the middle node of the LinkedList.
  * <p>
- * A happy number is a number defined by the following process:
- * <p>
- * Starting with any positive integer, replace the number by the sum of the squares of its digits.
- * Repeat the process until the number equals 1 (where it will stay),
- * or it loops endlessly in a cycle which does not include 1.
- * Those numbers for which this process ends in 1 are happy.
- * Return true if n is a happy number, and false if not.
- * <p>
- * Example 1:
- * Input: n = 19
- * Output: true
- * Explanation:
- * 1**2 + 9**2 = 82
- * 8**2 + 2**2 = 68
- * 6**2 + 8**2 = 100
- * 1**2 + 0**2 + 0**2 = 1
+ * If the total number of nodes in the LinkedList is even, return the second middle node.
  */
 class Solution {
-    public static int count(int n) {
-        int res = 0;
-        while (n != 0) {
-            res += (n % 10) * (n % 10);
-            n = n / 10;
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
         }
-        return res;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+
+        public void addL(int n) {
+            if (n < 1) return;
+            this.val = n;
+            this.next = new ListNode();
+            this.next.addL(n - 1);
+        }
+
+        @Override
+        public String toString() {
+            if (this.next == null) {
+                return this.val + " ";
+            }
+            return this.val + " " + this.next;
+        }
     }
 
-    public static boolean isLuckyNumber(int n) {
-        int turt = n, rabb = n; //алгоритм кролика и черепахи
-        do {
-            turt = count(turt);
-            if (turt == 1) return true;
-            rabb = count(count(rabb));
+    public static ListNode middleNode(ListNode head) {
+        ListNode turt, rabb;
+        turt = head;
+        rabb = head;
+        while (true) {
+            if (rabb.next == null) return turt;
+            else if (rabb.next.next == null)             return turt.next;
+            turt = turt.next;
+            rabb = rabb.next.next;
         }
-        while (turt != rabb); //если в последовательности есть цикл, то они в нём встретятся в какой-то момент
-        return false;
-
     }
 
 
     public static void main(String[] args) {
-        System.out.println(Solution.isLuckyNumber(23));
+        ListNode node = new ListNode();
+        node.addL(6);
+        System.out.println(node);
+        System.out.println(Solution.middleNode(node).val);
+
     }
 }
