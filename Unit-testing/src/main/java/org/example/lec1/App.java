25be8288f450d3e5beea2f7d6399b4892bd9f168
Pablos <pablos@list.ru>
2023-12-16 21:08:48 +0300

    Unit-testing lec1 fin

--- Unit-testing/src/main/java/org/example/lec1/App.java
+++ Unit-testing/src/main/java/org/example/lec1/App.java
@@ -9,7 +9,8 @@ public class App {
         assertThat(App.countIt(2, 6, "+")).isEqualTo(8);
         assertThat(App.countIt(2, 2, "-")).isEqualTo(0);
         assertThat(App.countIt(2, 7, "*")).isEqualTo(14);
-        assertThat(App.countIt(100, 50, "/")).isEqualTo(2);
+        // .as(сообщение) будет выводиться перед ошибкой
+        assertThat(App.countIt(100, 50, "/")).as("не делится так!").isEqualTo(1);
 
         assert App.countIt(2, 6, "+") == 8;
 
