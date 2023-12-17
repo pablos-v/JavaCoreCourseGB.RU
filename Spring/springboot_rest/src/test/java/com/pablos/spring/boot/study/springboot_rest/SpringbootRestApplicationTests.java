eed4b697b42b54a62f7f5401aca6c4f7d514fbbb
Pablos <pablos@list.ru>
2023-12-17 19:49:05 +0300

    Spring Boot REST API init

---
+++ Spring/springboot_rest/src/test/java/com/pablos/spring/boot/study/springboot_rest/SpringbootRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablos.spring.boot.study.springboot_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
