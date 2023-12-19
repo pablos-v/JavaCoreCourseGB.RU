8566313d0cf996b5a2060d64f591a28b80af493c
Pablos <pablos@list.ru>
2023-12-19 15:06:28 +0300

    SPRING BOOT REST autogeneration _supplemented by the method findAllByNameOrSalary_

--- Spring/springbootrest/src/main/java/com/pablos/springbootrest/dao/EmployeeRepository.java
+++ Spring/springbootrest/src/main/java/com/pablos/springbootrest/dao/EmployeeRepository.java
@@ -2,7 +2,13 @@ package com.pablos.springbootrest.dao;
 
 import com.pablos.springbootrest.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    @RestResource(path = "names", rel = "names")
+    List<Employee> findAllByNameOrSalary(@Param("name") String name, @Param("salary") Integer salary);
 }
