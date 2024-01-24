14e5b588362c0080a99c35c4fe1c768ad1613aba
Pablos <pablos@list.ru>
2024-01-24 18:30:16 +0300

    Client_for_Spring_REST_API template

--- Spring/Client_for_Spring_REST_API/src/main/java/org/pablos/spring/rest/client/Communication.java
+++ Spring/Client_for_Spring_REST_API/src/main/java/org/pablos/spring/rest/client/Communication.java
@@ -11,7 +11,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 /**
- * это класс для соединения клиента с сервером
+ * это класс для соединения клиента с сервером!
  */
 @Component
 public class Communication {
